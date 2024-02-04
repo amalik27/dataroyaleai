8d303dcf9d09b2f33741fef14dc2e05f44d3f4f1
Ruben Alias <34107608+Herxity@users.noreply.github.com>
2024-02-03 20:01:40 -0500

    Experiment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var shell = require('shelljs');
 
-shell.exec("sudo docker build -t flask ./dockercontainer")
-shell.exec("sudo docker run -d -p 5000:5000 flask")
+//Can use some DSA for the automatic local port assignment within 5000-5010
+
+shell.exec("docker build -t user1 ./dockercontainer")
+shell.exec("docker build -t user2 ./dockercontainer")
+shell.exec("docker build -t user3 ./dockercontainer")
+
+shell.exec("docker run -d -p 5000:5000 user1") 
+shell.exec("docker run -d -p 5001:5000 user2")
+shell.exec("docker run -d -p 5002:5000 user3")
