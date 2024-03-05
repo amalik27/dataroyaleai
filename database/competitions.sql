488710eac23d85e6e9f385effe584591942af50a
deshnadoshi <deshna.doshi@gmail.com>
2024-03-05 17:04:49 -0500

    Competition Management DB Set-up

---
+++ database/competitions.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 05, 2024 at 09:58 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `swe2024`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `competitions`
+--
+
+CREATE TABLE `competitions` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `deadline` date NOT NULL,
+  `prize` int(11) NOT NULL,
+  `description` varchar(6000) NOT NULL,
+  `player-cap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `competitions`
+--
+ALTER TABLE `competitions`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
