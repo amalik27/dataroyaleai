ab63932864eae559ba57eb6164a76e18747faf15
Ruben Alias <34107608+Herxity@users.noreply.github.com>
2024-03-11 20:22:43 -0400

    Necessary comment on how bad the naming scheme is somewhere

--- api-platform/daemon.js
+++ api-platform/daemon.js
@@ -176,9 +176,9 @@ class PrometheusDaemon{
   killContainers(containers, silent = true){
     console.log(chalk.red("[Prometheus] Killing Containers..."));
     containers.forEach((container) => {
-        let containerID = container.containerID
-        let dockerPsOutput = shell.exec(`docker ps | grep ${containerID} | cut -f 1 -d ' '`, {silent: silent});
-        if (dockerPsOutput.code !== 0) {
+        let containerTag = container.containerID//I know this is terrible
+        let containerID = shell.exec(`docker ps | grep ${containerTag} | cut -f 1 -d ' '`, {silent: silent});
+        if (containerID.code !== 0) {
             console.log(chalk.red(`Error finding running container for ID: ${containerID}`));
             return; // Exit this iteration of the loop and continue with the next
         }
